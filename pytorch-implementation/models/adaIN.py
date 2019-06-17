484d079b4846a2a7ee06dec5c9bd83ba59cb33a0
hoanghihi <a2k40pbc@gmail.com>
2019-06-17 12:53:21 +0700

    added module AdaIN

--- pytorch-implementation/models/adaIN.py
+++ pytorch-implementation/models/adaIN.py
@@ -1,7 +1,30 @@
 import torch
 import torch.nn as nn
-import torchvision
+import torch.nn.functional as F
 
 class AdaptiveInstanceNormalization(nn.Module):
-    def __init__(self):
+    def __init__(self, num_features, eps=1e-5, momentum=0.1):
         super(AdaptiveInstanceNormalization, self).__init__()
+        self.eps = eps
+        self.num_features = num_features
+        self.momentum = momentum
+        self.weight = None
+        self.bias = None
+        self.register_buffer('running_mean', torch.zeros(num_features))
+        self.register_buffer('running_var', torch.ones(num_features))
+
+    def forward(self, x):
+        assert self.weight is not None and self.bias is not None, "assign weight and bias before calling AdaIN"
+        b, c = x.size(0), x.size(1)
+        running_mean = self.running_mean.repeat(b)
+        running_var = self.running_var.repeat(b)
+        x_reshaped = x.contiguous().view(1, b*c, *x.size()[2:])
+        out = F.batch_norm(
+            x_reshaped, running_mean, running_var, self.weight, self.bias,
+            True, self.momentum, self.eps
+        )
+
+        return out.view(b,c,*x.size[2:])
+
+
+
